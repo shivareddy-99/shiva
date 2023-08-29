479004bd70fb55ec45a2f9229145c28e50a7fe66
shivareddy-99 <143476142+shivareddy-99@users.noreply.github.com>
2023-08-29 10:32:45 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Division
+{
+    public static void main(String args[]) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter first integer ");
+            int i1=sc.nextInt();
+            System.out.println("Enter second integer ");
+            int i2=sc.nextInt();
+            int i3=i1/i2;
+            System.out.println("divi of "+i1+" and "+i2+" is "+i3);
+    }
+}+
